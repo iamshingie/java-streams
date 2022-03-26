0323882a21d676074b34ed4d20ca0447a6d19a06
iamshingiegitgit <shingiraideke@gmail.com>
2022-03-26 13:00:06 +0200

    streamAPI

---
+++ src/StreamsAPI.java
@@ -0,0 +1,49 @@
+import java.util.Comparator;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+public class StreamsAPI {
+    public static void main(String[] args) {
+
+        //Declarative Approach
+
+        List<Person> people = getPeople();
+
+        //Filter
+        List<Person> females = people.stream()
+                .filter(person -> person.getGender().equals(Gender.FEMALE))
+                .collect(Collectors.toList());
+        females.forEach(System.out::println);
+
+        //Min
+        people.stream()
+                .min(Comparator.comparing(Person::getAge))
+                .ifPresent(System.out::println);
+
+        //Sort
+        List<Person> sorted = people.stream()
+                .sorted(Comparator.comparing(Person::getAge).thenComparing(Person::getGender).reversed())
+                .collect(Collectors.toList());
+        sorted.forEach(System.out::println);
+
+        Optional<String> oldestFemaleAge = people.stream()
+                .filter(person -> person.getGender().equals(Gender.FEMALE))
+                .max(Comparator.comparing(Person::getAge))
+                .map(Person::getName);
+
+        oldestFemaleAge.ifPresent(System.out::println);
+    }
+
+    private static List<Person> getPeople() {
+        return List.of(
+                new Person("Antonio", 20, Gender.MALE),
+                new Person("Alina Smith", 33, Gender.FEMALE),
+                new Person("Helen White", 57, Gender.FEMALE),
+                new Person("Alex Boz", 14, Gender.MALE),
+                new Person("Jamie Goa", 99, Gender.MALE),
+                new Person("Anna Cook", 7, Gender.FEMALE),
+                new Person("Zelda Brown", 120, Gender.FEMALE)
+        );
+    }
+}
